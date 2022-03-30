94956d45146938ae6416e35cc7569215a1547d0d
Henke <catears13@gmail.com>
2022-03-30 23:01:51 +0200

    Update build number to reflect demo site

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import shutil
 
-docker_tag = 'wedding-server:v0.0.1-e'
+docker_tag = 'wedding-server:v0.0.1-f'
 
 def get_relative_directory(folder_name: str) -> str:
     top_directory = os.path.dirname(os.path.realpath(__file__))
