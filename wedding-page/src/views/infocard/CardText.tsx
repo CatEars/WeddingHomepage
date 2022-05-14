47f56a2620a7ae2c6a09604b27842940990e6e24
Henrik 'Catears' Adolfsson <catears13@gmail.com>
2022-05-14 11:00:09 +0200

    Format CardText component

--- wedding-page/src/views/infocard/CardText.tsx
+++ wedding-page/src/views/infocard/CardText.tsx
@@ -1,39 +1,45 @@
-import React from 'react'
+import React from "react";
 import { Box, Typography } from "@mui/material";
 import { InfoCardMessage, ComplexMessage } from "../../text-content";
 
 type ComplexTextTypographyProps = {
-    message: ComplexMessage[][]
-}
+    message: ComplexMessage[][];
+};
 
 const ComplexTextTypography = (props: ComplexTextTypographyProps) => {
-    return (
-        <>{props.message}</>    
-    )    
-}
+    return <>{props.message}</>;
+};
 
 type CardTextProps = {
     header: string;
     message: InfoCardMessage;
-}
+};
 
-const CardText = (props: CardTextProps) => (
-    <Box>
-        <Typography variant="h2" sx={{ my: 3 }}>
-            {props.header}
-        </Typography>
-        {Array.isArray(props.message) ? (
-            props.message.map((msg, idx) => (<Typography sx={{ my: idx === 0 ? 0 : 2 }} variant="body1" textAlign="center">
-                {msg}
-            </Typography>))
-        ) : (typeof props.message === 'string') ? (
-            <Typography sx={{ my: 0 }} variant="body1" textAlign="center">
-                {props.message}
+const CardText = (props: CardTextProps) => {
+    return (
+        <Box>
+            <Typography variant="h2" sx={{ my: 3 }}>
+                {props.header}
             </Typography>
-        ) : (
-            <ComplexTextTypography message={props.message} />
-        )}
-    </Box>    
-)
+            {Array.isArray(props.message) ? (
+                props.message.map((msg, idx) => (
+                    <Typography
+                        sx={{ my: idx === 0 ? 0 : 2 }}
+                        variant="body1"
+                        textAlign="center"
+                    >
+                        {msg}
+                    </Typography>
+                ))
+            ) : typeof props.message === "string" ? (
+                <Typography sx={{ my: 0 }} variant="body1" textAlign="center">
+                    {props.message}
+                </Typography>
+            ) : (
+                <ComplexTextTypography message={props.message} />
+            )}
+        </Box>
+    );
+};
 
 export default CardText;
